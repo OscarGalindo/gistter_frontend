808421e1d0b72a6d582a4b51f3d9c9430bfb1ecb
Naman <namanv91@gmail.com>
2015-05-11 19:18:26 +0200

    validation

--- js/app.js
+++ js/app.js
@@ -1,15 +1,36 @@
-// create angular module
-var app = angular.module('validation', []);
+(function() {
+    // create angular module
+    var app = angular.module('validation', []);
 
-// create angular controller
-app.controller('FormController', function($scope) {
+    // create angular controller
+    app.controller('FormController', function($scope) {
 
-    // function to submit the form after all validation has occurred
-    $scope.signinForm = function(isValid) {
+        // function to submit the form after all validation has occurred
+        $scope.submitForm = function(isValid) {
 
-        // check to make sure the form is completely valid
-        if (isValid) {
-            alert('Fucking Bitches!!!!');
-        }
-    };
-});
+            // check to make sure the form is completely valid
+            if (isValid) {
+                alert('Fucking Bitches!!!!');
+            }
+        };
+    });
+
+    app.directive("compareTo", function(){
+        return {
+            require: "ngModel",
+            scope: {
+                otherModelValue: "=compareTo"
+            },
+            link: function(scope, element, attributes, ngModel) {
+
+                ngModel.$validators.compareTo = function(modelValue) {
+                    return modelValue == scope.otherModelValue;
+                };
+
+                scope.$watch("otherModelValue", function() {
+                    ngModel.$validate();
+                });
+            }
+        };
+    });
+}());
