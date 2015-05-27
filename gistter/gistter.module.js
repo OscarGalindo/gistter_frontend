b765ce06630a5b90a2a930838d7a61f6d0e9a68d
kaseoga <galindero@gmail.com>
2015-05-27 22:35:05 +0200

    MomentJS insertado

--- gistter/gistter.module.js
+++ gistter/gistter.module.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('gistter', ['ui.router', 'angular-jwt', 'ngStorage', 'picardy.fontawesome', 'ngSanitize'])
+        .module('gistter', ['ui.router', 'angular-jwt', 'ngStorage', 'picardy.fontawesome', 'ngSanitize', 'angularMoment'])
         .run(function($rootScope, $state) {
             $rootScope.$on('$stateChangeError', function() {
                 $state.go('404');
