42f3a24cda1470b37864dd1caa998bd1e45b81b0
kaseoga <galindero@gmail.com>
2015-05-27 21:54:38 +0200

    Gulp notify para comprobar cada vez que se compila el JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var notify = require('gulp-notify');
 
 gulp.task('js', function () {
     gulp.src(['gistter/**/*.module.js', 'gistter/**/*.js'])
         .pipe(concat('build/gistter.js'))
         .pipe(gulp.dest('.'))
+        .pipe(notify('Compilado JS'))
 });
 
 gulp.task('watch', ['js'], function () {
     gulp.watch('gistter/**/*.js', ['js'])
 });
+
+gulp.task('default', ['watch']);
