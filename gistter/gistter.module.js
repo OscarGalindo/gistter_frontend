421eb422e68379d004bc93baec4007d706c3fa77
kaseOga <galindero@gmail.com>
2015-05-28 20:34:07 +0200

    Initial ngAnimate

--- gistter/gistter.module.js
+++ gistter/gistter.module.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('gistter', ['ui.router', 'angular-jwt', 'ngStorage', 'picardy.fontawesome', 'ngSanitize', 'angularMoment'])
+        .module('gistter', ['ui.router', 'angular-jwt', 'ngStorage', 'picardy.fontawesome', 'ngSanitize', 'angularMoment', 'ngAnimate'])
         .run(function($rootScope, $state) {
             $rootScope.$on('$stateChangeError', function() {
                 $state.go('404');
