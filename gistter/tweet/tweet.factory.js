d4de896b0184d8482ed5460d72771407e90b41c6
kaseOga <galindero@gmail.com>
2015-05-29 19:31:33 +0200

    Responder tweet

--- gistter/tweet/tweet.factory.js
+++ gistter/tweet/tweet.factory.js
@@ -5,14 +5,15 @@
         .module('gistter')
         .factory('tweetFactory', tweet);
 
-    tweet.$inject = ['API', '$http'];
+    tweet.$inject = ['API', '$http', '$q'];
 
     /* @ngInject */
-    function tweet(API, $http) {
+    function tweet(API, $http, $q) {
         var service = {
             tweet: tweet,
             create: create,
-            get_by_id: get_by_id
+            get_by_id: get_by_id,
+            reply: reply
         };
 
         return service;
@@ -28,6 +29,22 @@
 
         function get_by_id(id) {
             return $http.get(API.url + 'tweet/' + id);
+        }
+
+        function reply(id_parent, body) {
+            var q = $q.defer();
+
+            var reply = {
+                body: body,
+                id_parent: id_parent
+            };
+
+            service.create(reply)
+                .success(function(data) {
+                    q.resolve(data);
+                });
+
+            return q.promise;
         }
     }
 })();
