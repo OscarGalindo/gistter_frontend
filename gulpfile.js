25c8a06371b6f646b733561ebee2aff280e20d17
kaseoga <galindero@gmail.com>
2015-06-02 00:05:32 +0200

    Gulp to production

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var notify = require('gulp-notify');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
 gulp.task('js', function () {
     gulp.src(['gistter/**/*.module.js', 'gistter/**/*.js'])
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
         .pipe(concat('build/gistter.js'))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('.'))
         .pipe(notify('Compilado JS'))
 });
